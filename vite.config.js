776f5ce34c487b87019d402cc86a8cabd393b242
혜준 <junjunpark08@gmail.com>
2026-01-13 00:05:23 +0900

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/junspace_vue/', // ← 레포 이름으로 바꾸기
+  server: {
+    open: true,
+  },
+  base: '/junspace_vue/',
 })
